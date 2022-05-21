a11b7aab3a6b75f9d435f036e226ae2e66c84bc6
FerTorron <100718699+FerTorron@users.noreply.github.com>
2022-05-21 14:05:01 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,11 +11,11 @@ if (toastTrigger) {
 
 // ScrollReveal
 ScrollReveal().reveal('.navbar');
-ScrollReveal().reveal('.carousel', {delay: 500});
-ScrollReveal().reveal('.container-precios', {delay: 500});
-ScrollReveal().reveal('.burger1', {delay: 200});
-ScrollReveal().reveal('.burger2', {delay: 400});
-ScrollReveal().reveal('.burger3', {delay: 600});
-ScrollReveal().reveal('.container-yt', {delay: 500});
-ScrollReveal().reveal('.container-map', {delay: 500});
-ScrollReveal().reveal('.container-footer', {delay: 500});
+ScrollReveal().reveal('.carousel', {delay: 200});
+ScrollReveal().reveal('.container-precios', {delay: 200});
+ScrollReveal().reveal('.burger1', {delay: 100});
+ScrollReveal().reveal('.burger2', {delay: 150});
+ScrollReveal().reveal('.burger3', {delay: 200});
+ScrollReveal().reveal('.container-yt', {delay: 200});
+ScrollReveal().reveal('.container-map', {delay: 200});
+ScrollReveal().reveal('.container-footer', {delay: 200});
